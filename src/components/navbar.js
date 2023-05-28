34486b8e9746640c3bd767cdaaafdcb4b9896d3d
dbianchii <gdbianchii@gmail.com>
2023-05-28 11:04:36 -0300

    Fix hamburguer 🍔

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -29,12 +29,6 @@ export default function NavBar() {
 					<img src="https://companieslogo.com/img/orig/SU.PA-386facc5.png?t=1648148280" className="mr-3 h-8" alt="Flowbite Logo" />
 					<span className="self-center whitespace-nowrap text-2xl font-semibold text-schneider-green">Schneider Connect</span>
 				</a>
-				<button data-collapse-toggle="navbar-default" type="button" className="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-400 transition-colors hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 md:hidden" aria-controls="navbar-default" aria-expanded="false">
-					<span className="sr-only">Open main menu</span>
-					<svg className="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-						<path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
-					</svg>
-				</button>
 				<div className="hidden w-full md:block md:w-auto" id="navbar-default">
 					<ul className="mt-4 flex flex-col rounded-lg border border-gray-700 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0  md:p-0">
 						{navItems.map((item, i) => (
@@ -72,16 +66,22 @@ export default function NavBar() {
 							</DropdownMenu.Portal>
 						</DropdownMenu.Root>
 					) : (
-						<>
+						<div className="hidden md:block">
 							<ActionButton isLink={true} href="/signIn">
 								Entrar
 							</ActionButton>
 							<SecondaryButton isLink={true} href="/signIn" className={"ml-4"}>
 								Cadastrar
 							</SecondaryButton>
-						</>
+						</div>
 					)}
 				</div>
+				<button data-collapse-toggle="navbar-default" type="button" className="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-400 transition-colors hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 md:hidden" aria-controls="navbar-default" aria-expanded="false">
+					<span className="sr-only">Open main menu</span>
+					<svg className="h-6 w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+						<path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
+					</svg>
+				</button>
 			</div>
 		</nav>
 	)
