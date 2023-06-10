9c1316675d32430e48c14fdf5250e2e474dc515b
dbianchii <gdbianchii@gmail.com>
2023-06-09 23:05:32 -0300

    Some fix

---
+++ README.md
@@ -0,0 +1,32 @@
+Projeto inicializado com Create-React-App para o trabalho de encerramento do primeiro ano de Engenharia de Software da FIAP
+
+- React
+- React-Hook-Form
+- Tailwind
+- React Icons
+- React Router DOM
+- React Hook form e Yup
+- Salvamento de dados em local storage
+
+LORENZO FERREIRA - RM97318
+LUCAS BENEDETTI - RM96771
+ENRICO POZZI - RM97327
+JOÃO GABRIEL RODRIGUEZ - RM96765
+GABRIEL BIANCHI - RM97325
+
+Caro professor / avaliador. Fique a vontade para navegar na plataforma. Você pode criar um usuário na página "/register", para melhor conseguir ver todos os recursos da plataforma.
+
+Segue aqui alguns cadastros de usuários mockados para conseguir acessar por diferentes perfis:
+(desta forma não precisa criar mais de um perfil para testar a interação de múltiplos usuários 😉)
+
+- Email: jiulia@schneider.com
+- Senha: EuGostoDeBolo123
+
+- Email: rodriguez@schneider.com
+- Senha: EuAmoReact!
+
+- Email: suellenNakamoto@schneider.com
+- Senha: EuGostoDeTailwind25!
+
+- Email: pedroRocini@schneider.com
+- Senha: EuNaoGostoDeBootstrap!23
--- src/Usuario.js
+++ src/Usuario.js
@@ -2,63 +2,55 @@ import { SchneiderAvatar } from "./components/avatar"
 import { api } from "./server/api/apiRoot"
 import { useParams } from "react-router-dom"
 import { useState } from "react"
-import { PostCard } from "./components/postCard";
+import { PostCard } from "./components/postCard"
 
 export default function Usuario() {
-  const params = useParams();
+	const params = useParams()
 
-  const user = api.user.getUser({ userId: params.userId });
-  const loggedUser = api.session.getLoggedUser();
+	const user = api.user.getUser({ userId: params.userId })
+	const loggedUser = api.session.getLoggedUser()
 
-  const [following, setFollowing] = useState(
-    user.followers.includes(loggedUser.id)
-  );
+	const [following, setFollowing] = useState(user.followers.includes(loggedUser?.id))
 
-  const posts = api.posts.getPostsForUser(user.id);
+	const posts = api.posts.getPostsForUser(user.id)
 
-  return (
-    <div className="flex min-h-[80vh] flex-row">
-      <div className="flex h-full w-4/6 flex-wrap justify-center gap-4 p-4">
-        {posts?.map((post, index) => (
-          <PostCard key={`postCardKey-${index}`} post={post} />
-        ))}
-      </div>
-      <div className="w-2/6 p-8">
-        <div className="flex w-24 flex-col">
-          <SchneiderAvatar src={user.image} size={"lg"} />
-          <h1 className="mt-2 font-semibold">{user.name}</h1>
-          <p className="text-sm text-gray-500">{user.email}</p>
-          <p className="mt-2 text-sm text-gray-500">
-            <span className="font-bold">{user.followers.length}</span>{" "}
-            {user.followers.length === 1 ? "seguidor" : "seguidores"}
-          </p>
+	return (
+		<div className="flex min-h-[80vh] flex-row">
+			<div className="flex h-full w-4/6 flex-wrap justify-center gap-4 p-4">
+				{posts?.map((post, index) => (
+					<PostCard key={`postCardKey-${index}`} post={post} />
+				))}
+			</div>
+			<div className="w-2/6 p-8">
+				<div className="flex w-24 flex-col">
+					<SchneiderAvatar src={user.image} size={"lg"} />
+					<h1 className="mt-2 font-semibold">{user.name}</h1>
+					<p className="text-sm text-gray-500">{user.email}</p>
+					<p className="mt-2 text-sm text-gray-500">
+						<span className="font-bold">{user.followers.length}</span> {user.followers.length === 1 ? "seguidor" : "seguidores"}
+					</p>
 
-          {loggedUser.id !== user.id && (
-            <button
-              className={`mt-4 rounded-md ring-2 ring-schneider-green ${
-                following
-                  ? " bg-schneider-green text-white"
-                  : "bg-schneider-green/30 text-gray-600"
-              }`}
-              size={"sm"}
-              onClick={() => {
-                try {
-                  if (loggedUser.id !== user.id) {
-                    if (following) api.user.unfollowUser(user.id);
-                    else api.user.followUser(user.id);
-                  }
-                  setFollowing(!following);
-                } catch (error) {
-                  if (error.message === "Usuario não logado")
-                    window.location.href = "/login";
-                }
-              }}
-            >
-              {following ? "Seguindo" : "Seguir"}
-            </button>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+					{loggedUser?.id !== user.id && (
+						<button
+							className={`mt-4 rounded-md ring-2 ring-schneider-green ${following ? " bg-schneider-green text-white" : "bg-schneider-green/30 text-gray-600"}`}
+							size={"sm"}
+							onClick={() => {
+								try {
+									if (loggedUser?.id !== user.id) {
+										if (following) api.user.unfollowUser(user.id)
+										else api.user.followUser(user.id)
+									}
+									setFollowing(!following)
+								} catch (error) {
+									if (error.message === "Usuario não logado") window.location.href = "/login"
+								}
+							}}
+						>
+							{following ? "Seguindo" : "Seguir"}
+						</button>
+					)}
+				</div>
+			</div>
+		</div>
+	)
 }
